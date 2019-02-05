abf102d78412608f4a3fa0f800cfeafe4de1906e
Alexandr Olferuk <olferuk@surfstudio.ru>
2019-02-05 11:21:08 +0300

    Setup changes

--- setup.py
+++ setup.py
@@ -3,9 +3,8 @@ from setuptools import setup
 setup(name='Spindrift',
       version='0.1',
       description='The tool to make lightweight Telegram bots from plain Python functions',
-      author='Alexander Olferuk, Danila Paluhin, Surf',
+      author='Alexander Olferuk and Danila Paluhin, Surf',
       license='MIT',
-      install_requires=["python-telegram-bot"],
+      install_requires=['python-telegram-bot', 'pandas'],
       packages=['spindrift']
 )
-
